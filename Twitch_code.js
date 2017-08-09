e1fec8cd2c10ebb13990241d3f976146c7800412
Dania801 <ssansnnom07@gmail.com>
2017-08-09 13:36:16 +0300

    Completing the functionality of the app

--- Twitch_code.js
+++ Twitch_code.js
@@ -1,20 +1,21 @@
-var streamers = ["dreamhackcs", "skyzhar", "freecodecamp", "faceittv", "comster404", "brunofin", "terakilobyte",
-                "robotcaleb", "sheevergaming", "esl_sc2", "ogamingsc2", "jacksofamerica"];
+var streamers = ["dreamhackcs", "skyzhar", "freecodecamp", "faceittv" , "esl_sc2", "ogamingsc2", "jacksofamerica"];
 
-var twitchURL = "https://api.twitch.tv/kraken/channels/" ;
+var channelURL = "https://api.twitch.tv/kraken/channels/" ;
 var streamingURL = "https://api.twitch.tv/kraken/streams/" ;
 var APIKey = "?client_id=n4w6p8y0b2csq0r972djznn089ojy2" ;
 
-function connectToStreamer(streamer)
-{
+
+
+
+function getOnlineStreamers(streamer){
   $.ajax({
-           url: twitchURL + streamer + APIKey ,
+           url: streamingURL + streamer + APIKey ,
            dataType: "jsonp",
            data: {
                format: "json"
            },
            success: function (data) {
-               getStreamerInfo(data);
+              getStreamerInfo(data , streamer);
            },
            error: function () {
                console.log("unable to access json");
@@ -22,20 +23,39 @@ function connectToStreamer(streamer)
        });
 }
 
-function getStreamingStatus(streamer)
-{
-  $.getJSON(streamingURL + streamer + APIKey, function(channel) {
+function getOfflineStreamers(streamer){
+  $.ajax({
+           url: streamingURL + streamer + APIKey ,
+           dataType: "jsonp",
+           data: {
+               format: "json"
+           },
+           success: function (data) {
+             if(data.stream == null){
+               $.ajax({
+                       url: channelURL + streamer + APIKey ,
+                       dataType: "jsonp" ,
+                       data: {
+                         format: "json"
+                       },
+                       success: function(data){
 
-    if (channel["stream"] == null) {
-        alert("yes") ; 
-    } else {
-       alert("die !") ;
-    }
-  });
+                         getChannelInfo(data) ;
+                       },
+                       error: function(){
+                         info.innerHTML = "ERROR" ;
+                       }
+               });
+             }
+           },
+           error: function () {
+               console.log("unable to access json");
+           }
+       });
 }
 
-function getStreamerInfo(data)
-{
+function getChannelInfo(data){
+
   var name = data.display_name ;
   var game = data.game ;
   var status = data.status ;
@@ -43,11 +63,64 @@ function getStreamerInfo(data)
   var views = data.views ;
   var followers = data.followers ;
   var logo = data.logo ;
-  getStreamingStatus("dreamhackcs") ;
-  var div = document.getElementById('info');
-  //div.innerHTML = stream ;
+  var lang = data.broadcaster_language ;
+  offline.innerHTML += '<li class="w3-padding-16 w3-red w3-animate-right">'+
+                    '<img src="'+logo+'" class="w3-left w3-circle w3-margin-right" style="width:50px"/>'+
+                    '<span class="w3-large">'+name+'</span><br>'+
+                    '<span>'+game+'</span>'+
+                    '</li>'
+}
+
+function getStreamerInfo(data , streamer)
+{
+
+
+  if(data.stream != null) {
+    var name = data.stream.channel.display_name ;
+    var game = data.stream.channel.game ;
+    var status = data.stream.channel.status ;
+    var url = data.stream.channel.url ;
+    var views = data.stream.channel.views ;
+    var followers = data.stream.channel.followers ;
+    var logo = data.stream.channel.logo ;
+    var lang = data.stream.channel.broadcaster_language ;
+    online.innerHTML += '<li class="w3-padding-16 w3-green w3-animate-left">'+
+                      '<img src="'+logo+'" class="w3-left w3-circle w3-margin-right" style="width:50px"/>'+
+                      '<span class="w3-large">'+name+'</span><br>'+
+                      '<span>'+game+'</span>'+
+                      '</li>'
+  }
+
+}
+
+function getAllUsers(){
+  for(var i = 0 ; i < streamers.length ; i++){
+    online.innerHTML = "" ;
+    offline.innerHTML = "" ;
+    getOnlineStreamers(streamers[i]) ;
+    getOfflineStreamers(streamers[i]) ;
+  }
+}
+
+function getOnlineUsers(){
+  online.innerHTML = "" ;
+  offline.innerHTML = "" ;
+  for(var i = 0 ; i < streamers.length ; i++){
+    getOnlineStreamers(streamers[i]) ;
+  }
+}
+
+function getOfflineUsers(){
+  online.innerHTML = "" ;
+  offline.innerHTML = "" ;
+  for(var i = 0 ; i < streamers.length ; i++){
+    getOfflineStreamers(streamers[i]) ;
+  }
 }
 
 $(document).ready(function(){
-  connectToStreamer("dreamhackcs") ;
+  for(var i = 0 ; i < streamers.length ; i++){
+    getOnlineStreamers(streamers[i]) ;
+    getOfflineStreamers(streamers[i]) ;
+  }
 })
